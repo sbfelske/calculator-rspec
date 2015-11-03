68eeea436e80e2fe0090f70d8550850bf65eb25f
Scott Felske <sbfelske@gmail.com>
2015-11-02 20:42:15 -0500

    Scott Felske calculator kata

--- lib/Calculator.rb
+++ lib/Calculator.rb
@@ -4,4 +4,15 @@ class Calculator
 	n1.inject(:+)
 	end
 
+	def sub(*n1)
+	n1.inject(:-)
+	end
+
+	def mult(*n1)
+	n1.inject(:*)
+	end
+
+	def div(*n1)
+	n1.inject(:/)
+	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,6 @@
 require 'rspec'
 require 'calculator'
 
-
 describe Calculator do
 
 	describe 'addition' do
@@ -12,4 +11,27 @@ describe Calculator do
 		end
 	end
 
+	describe 'subtraction' do
+		it 'returns an integer' do
+			calc= Calculator.new
+			actual = calc.sub(2,1)
+			expect(actual).to be_a Integer
+		end
+	end
+
+	describe 'multiplication' do
+		it 'returns an integer' do
+			calc = Calculator.new
+			actual = calc.mult(2,2)
+			expect(actual).to be_a Integer
+		end
+	end
+
+	describe 'division' do
+		it 'return an integer' do
+			calc = Calculator.new
+			actual = calc.div(2,2)
+			expect(actual).to be_a Integer
+		end
+	end
 end
